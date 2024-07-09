5113155522b5d2f7fdbd23f1f24fef998c1be415
Mose Müller <mosemueller@gmail.com>
2024-07-09 16:29:22 +0200

    monkey-patches dump and loads in pydase's socketio setup

--- src/icon/server/__main__.py
+++ src/icon/server/__main__.py
@@ -1,5 +1,11 @@
 import pydase
+import pydase.server.web_server.sio_setup
 
+import icon.serialization.deserializer
+import icon.serialization.serializer
 from icon.server.api.api_service import APIService
+
+pydase.server.web_server.sio_setup.loads = icon.serialization.deserializer.loads
+pydase.server.web_server.sio_setup.dump = icon.serialization.serializer.dump
 
 pydase.Server(APIService()).run()
