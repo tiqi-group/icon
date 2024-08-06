08f9b37cdf060633f200877d6dab6ad9e7e9b35f
Mose Müller <mosemueller@gmail.com>
2024-08-06 16:52:47 +0200

    adds ExperimentSourceRepository to get/create a new experiment source

---
+++ src/icon/server/data_access/repositories/experiment_source_repository.py
@@ -0,0 +1,38 @@
+import logging
+
+import sqlalchemy.orm
+
+from icon.server.data_access.db_context.sqlite import engine
+from icon.server.data_access.models.sqlite.experiment_source import ExperimentSource
+
+logger = logging.getLogger(__name__)
+
+
+class ExperimentSourceRepository:
+    @staticmethod
+    def get_or_create_experiment(
+        *,
+        experiment_source: ExperimentSource,
+    ) -> ExperimentSource:
+        """Gets the data of an already existing instance in the database or creates a
+        new instance if it does not exist, and returns this instance.
+        """
+
+        with sqlalchemy.orm.Session(engine) as session:
+            # Check if the experiment exists
+            experiment = (
+                session.query(ExperimentSource)
+                .filter_by(
+                    name=experiment_source.name, file_path=experiment_source.file_path
+                )
+                .first()
+            )
+
+            # If it doesn't exist, create it
+            if not experiment:
+                experiment = experiment_source
+                session.add(experiment)
+                session.commit()
+                session.refresh(experiment)  # Refresh to get the ID
+                logger.debug("Inserted new experiment %s", experiment)
+        return experiment
