1612003c282f2bc445c8021ffc19b5823bbe2c2e
Mose Müller <mosemueller@gmail.com>
2024-08-08 15:06:21 +0200

    updates pre_processing task

--- src/icon/server/pre_processing/task.py
+++ src/icon/server/pre_processing/task.py
@@ -1,14 +1,22 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pydantic
+
+if TYPE_CHECKING:
+    from datetime import datetime
 
 
 class PreProcessingTask(pydantic.BaseModel):
     name: str
-    priority: int
-    timestamp: float
-    no_data_points: int
-    git_hash: str | None = None
+    experiment_file_path: str
+    experiment_name: str
+    git_commit_hash: str | None = None
+    priority: int = pydantic.Field(ge=0, le=20)
+    local_parameters_timestamp: datetime
+    # scan_parameters: list[int]
+    auto_calibration: bool
     debug_mode: bool = False
 
     def __lt__(self, other: PreProcessingTask) -> bool:
