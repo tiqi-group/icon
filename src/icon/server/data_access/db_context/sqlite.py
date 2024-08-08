4e11067dbb0a1cafa62d0a765bf114316afad4f2
Mose Müller <mosemueller@gmail.com>
2024-08-08 09:45:39 +0200

    updates sqlite context (configurable through env variables)

--- src/icon/server/data_access/db_context/sqlite.py
+++ src/icon/server/data_access/db_context/sqlite.py
@@ -1,10 +1,18 @@
+import pydase.config
 import sqlalchemy
 
-from icon.server.data_access.models.sqlite.base import Base
+from icon.server.data_access.models.sqlite import Base
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-engine = sqlalchemy.create_engine(sqlite_url, echo=False)
+if pydase.config.OperationMode().environment == "testing":
+    SQLITE_FILE = "icon_testing.db"
+elif pydase.config.OperationMode().environment == "development":
+    SQLITE_FILE = "icon_dev.db"
+else:
+    SQLITE_FILE = "icon.db"
+
+
+SQLITE_URL = f"sqlite:///{SQLITE_FILE}"
+engine = sqlalchemy.create_engine(SQLITE_URL, echo=False)
 
 
 def create_db_and_tables() -> None:
