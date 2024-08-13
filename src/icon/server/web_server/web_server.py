2b540b11f9b23fad31967a95967be55dd5a9795a
Mose Müller <mosemueller@gmail.com>
2024-08-13 08:34:19 +0200

    updates sio events for data streaming

--- src/icon/server/web_server/web_server.py
+++ src/icon/server/web_server/web_server.py
@@ -5,6 +5,11 @@ import pydase.data_service.state_manager
 import pydase.server.web_server.sio_setup
 import socketio  # type: ignore
 
+from icon.serialization.serializer import dump
+from icon.server.data_access.repositories.experiment_data_repository import (
+    ExperimentDataRepository,
+)
+
 logger = logging.getLogger(__name__)
 
 pydase_setup_sio_events = pydase.server.web_server.sio_setup.setup_sio_events
@@ -21,33 +26,35 @@ def setup_sio_events(
     pydase_setup_sio_events(sio, state_manager)
 
     @sio.event  # type: ignore
-    async def get_experiment_data(sid: str, experiment_id: str) -> None:
+    async def get_experiment_data(sid: str, job_id: int) -> None:
         # First join room if exists to subscribe to updates
-        if experiment_id in sio.manager.rooms:
-            logger.debug(
-                "Client [%s] joined room %s",
-                click.style(str(sid), fg="cyan"),
-                experiment_id,
-            )
-            await sio.enter_room(sid, experiment_id)
-        else:
-            logging.debug(
-                "Client [%s] requested to join room %s but it does not exist.",
-                click.style(str(sid), fg="cyan"),
-                experiment_id,
-            )
+        job_room = f"experiment_{job_id}"
+        logger.debug(
+            "Client [%s] joined room %s",
+            click.style(str(sid), fg="cyan"),
+            job_room,
+        )
+        await sio.enter_room(sid, job_room)
 
         # Then serve all the data that has been collected already
         # The client will not miss anything then.
         await sio.emit(
-            "experiment_data", get_data_by_experiment_id(experiment_id), to=sid
+            "experiment_data",
+            {
+                "job_id": job_id,
+                "data": dump(
+                    ExperimentDataRepository.get_experiment_data_by_id(job_id)
+                ),
+            },
+            to=sid,
         )
 
     @sio.event  # type: ignore
-    async def stop_experiment_data_stream(sid: str, experiment_id: str) -> None:
-        logging.debug(
+    async def stop_experiment_data_stream(sid: str, job_id: int) -> None:
+        job_room = f"experiment_{job_id}"
+        logger.debug(
             "Client [%s] left room %s",
             click.style(str(sid), fg="cyan"),
-            experiment_id,
+            job_room,
         )
-        await sio.leave_room(sid, experiment_id)
+        await sio.leave_room(sid, job_room)
