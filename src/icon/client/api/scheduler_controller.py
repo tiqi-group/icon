c4a2ff82e479cda2c8a17fcacfdedc6f4eddefb3
Mose Müller <mosemueller@gmail.com>
2024-08-13 16:55:43 +0200

    adds a scheduler controller (and JobProxy) to the client

--- src/icon/client/api/scheduler_controller.py
+++ src/icon/client/api/scheduler_controller.py
@@ -1,6 +1,7 @@
+import asyncio
 import logging
 from datetime import datetime
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from icon.server.api.models.experiment import Experiment
 
@@ -9,6 +10,33 @@ if TYPE_CHECKING:
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+class JobProxy:
+    def __init__(self, *, client: "Client", job_id: int) -> None:
+        self._client = client
+        self._job_id = job_id
+        self._getting_data = False
+
+    def toggle_plot(self) -> None:
+        if not self._getting_data:
+            connection_future = asyncio.run_coroutine_threadsafe(
+                self._subscribe_to_experiment_data_stream(), self._client._loop
+            )
+            connection_future.result()
+        else:
+            connection_future = asyncio.run_coroutine_threadsafe(
+                self._unsubscribe_from_experiment_data_stream(), self._client._loop
+            )
+            connection_future.result()
+
+    async def _unsubscribe_from_experiment_data_stream(self) -> None:
+        await self._client._sio.emit("stop_experiment_data_stream", self._job_id)
+        self._getting_data = False
+
+    async def _subscribe_to_experiment_data_stream(self) -> None:
+        await self._client._sio.emit("get_experiment_data", self._job_id)
+        self._getting_data = True
 
 
 class SchedulerController:
@@ -22,8 +50,8 @@ class SchedulerController:
         priority: int,
         local_parameters_timestamp: datetime,
         # scan_info: ScanInfo,
-    ) -> Any:
-        return self._client.trigger_method(
+    ) -> JobProxy:
+        job_id: int = self._client.trigger_method(
             "scheduler.submit_job",
             kwargs={
                 "experiment": experiment,
@@ -32,3 +60,4 @@ class SchedulerController:
                 # "scan_info": scan_info,
             },
         )
+        return JobProxy(client=self._client, job_id=job_id)
