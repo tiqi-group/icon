d3b7c3b3c2ab6ddffa7a0a5e7efc493d8e6e70b2
Mose Müller <mosemueller@gmail.com>
2024-11-28 16:52:50 +0100

    adds task to ExperimentsController to periodically get experiment metadata

--- src/icon/server/api/experiments_controller.py
+++ src/icon/server/api/experiments_controller.py
@@ -1,11 +1,17 @@
 import asyncio
-from typing import Any, ClassVar
+import logging
+from typing import ClassVar
 
 import pydase
 from pydase.task.decorator import task
 
 from icon.config import get_config
 from icon.server.api.models.experiment import Experiment
+from icon.server.data_access.repositories.experiments_repository import (
+    ExperimentsRepository,
+)
+
+logger = logging.getLogger(__file__)
 
 
 class ExperimentsController(pydase.DataService):
@@ -13,3 +19,12 @@ class ExperimentsController(pydase.DataService):
 
     def get_experiments(self) -> list[Experiment]:
         return self._experiments
+
+    @task(autostart=True)
+    async def _update_experiment_cache(self) -> None:
+        while True:
+            await self._update_experiment_metadata()
+            await asyncio.sleep(get_config().experiment_library.update_interval)
+
+    async def _update_experiment_metadata(self) -> None:
+        await ExperimentsRepository.get_experiments()
