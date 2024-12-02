310be548dc28eed7f5896d0ff404156e9d22f8be
Mose Müller <mosemueller@gmail.com>
2024-12-02 15:21:42 +0100

    flattens experiment data structure in pycrystal experiments template

--- src/icon/server/data_access/templates/get_pycrystal_experiments.py
+++ src/icon/server/data_access/templates/get_pycrystal_experiments.py
@@ -23,10 +23,10 @@ experiments: ExperimentDict = {}
 for mod_info in pkgutil.iter_modules(experiment_library.experiments.__path__):
     experiment_module = experiment_library.experiments.__name__ + "." + mod_info.name
     config = get_config_from_module_name(experiment_module)
-    experiments[experiment_module] = {}
     for experiment, experiment_instance_kwargs in config["experiment_instances"]:
         experiment_instance_name = experiment_instance_kwargs["name"]
-        experiments[experiment_module][experiment_instance_name] = {
+        experiment_identifier = f"{experiment_module}.{experiment.__name__} ({experiment_instance_name})"
+        experiments[experiment_identifier] = {
             "class_name": experiment.__name__,
             "constructor_kwargs": experiment_instance_kwargs,
             "parameters": get_experiment_instance_display_groups(
