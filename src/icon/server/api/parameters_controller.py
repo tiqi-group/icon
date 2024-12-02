8219f368be4bb0584720488e142c6dd2ecc16fd3
Mose Müller <mosemueller@gmail.com>
2024-12-02 16:58:23 +0100

    updates ParametersController (mostly copied from ExperimentsController)

--- src/icon/server/api/parameters_controller.py
+++ src/icon/server/api/parameters_controller.py
@@ -1,5 +1,28 @@
+import logging
+from typing import Any
+
 import pydase
+
+from icon.server.data_access.repositories.parameter_metadata_repository import (
+    ParameterMetadataRepository,
+)
+
+logger = logging.getLogger(__file__)
 
 
 class ParametersController(pydase.DataService):
-    pass
+    async def get_experiments(self) -> dict[str, str]:
+        return await ParameterMetadataRepository.get_parameter_metadata()
+
+    async def _update_parameter_metadata(
+        self, parameter_metadata: dict[str, Any]
+    ) -> None:
+        logger.debug("Updating experiment metadata...")
+
+        (
+            added_exps,
+            removed_exps,
+            updated_exps,
+        ) = await ParameterMetadataRepository.update_parameter_metadata(
+            new_parameter_metadata=parameter_metadata
+        )
