4eaf09af318e4c895c7c46eb516f66062a5d6160
Mose Müller <mosemueller@gmail.com>
2024-12-03 14:51:29 +0100

    updates ParametersRepository

--- src/icon/server/data_access/repositories/parameters_repository.py
+++ src/icon/server/data_access/repositories/parameters_repository.py
@@ -1,18 +1,31 @@
 import json
-from pathlib import Path
-from typing import Any
 
-from icon.server.data_access.repositories.experiments_repository import (
-    ExperimentsRepository,
-)
+from icon.server.data_access.db_context.valkey import ValkeySession
+
+ValkeyValueType = bytes | str | float
 
 
 class ParametersRepository:
     @staticmethod
-    async def get_parameter_metadata() -> dict[str, Any]:
-        """Retrieve the experiments dictionary."""
-        code = ExperimentsRepository._get_code(
-            Path(__file__).parent.parent / "templates/get_pycrystal_parameters.py"
+    async def get_parameters() -> dict[str, ValkeyValueType]:
+        async with ValkeySession() as valkey:
+            params_serialized = await valkey.hgetall("parameters")  # type: ignore
+        return {key: json.loads(value) for key, value in params_serialized.items()}
+
+    @staticmethod
+    async def get_parameter_by_id(parameter_id: str) -> ValkeyValueType:
+        async with ValkeySession() as valkey:
+            return await valkey.hget("parameters", key=parameter_id)  # type: ignore
+
+    @staticmethod
+    async def update_parameter(parameter_id: str, new_value: ValkeyValueType) -> None:
+        await ParametersRepository.update_parameters(
+            parameter_mapping={parameter_id: new_value}
         )
-        stdout = await ExperimentsRepository._run_code(code)
-        return json.loads(stdout)
+
+    @staticmethod
+    async def update_parameters(parameter_mapping: dict[str, ValkeyValueType]) -> None:
+        async with ValkeySession() as valkey:
+            await valkey.hset("parameters", mapping=parameter_mapping)  # type: ignore
+
+        # TODO: update influxdb
