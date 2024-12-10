0cab1927f1de38a1881995646a938e851bc7eb3d
Mose Müller <mosemueller@gmail.com>
2024-12-10 15:42:47 +0100

    client: updates client (inheriting from pydase.Client)

--- src/icon/client/client.py
+++ src/icon/client/client.py
@@ -1,13 +1,12 @@
 import asyncio
 import logging
-import sys
-import threading
 from io import StringIO
-from types import TracebackType
 from typing import TYPE_CHECKING, Any, TypedDict
 
 import pandas as pd
-import socketio  # type: ignore
+import pydase
+import pydase.client.proxy_loader
+import socketio  # type: ignore[import-untyped]
 
 from icon.client.api.scheduler_controller import SchedulerController
 from icon.serialization.deserializer import loads
@@ -15,12 +14,6 @@ from icon.serialization.serializer import dump
 
 if TYPE_CHECKING:
     from icon.serialization.types import SerializedIconObject
-
-if sys.version_info < (3, 11):
-    from typing_extensions import Self
-else:
-    from typing import Self
-
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -31,46 +24,98 @@ class ExperimentData(TypedDict):
     data: str
 
 
-def asyncio_loop_thread(loop: asyncio.AbstractEventLoop) -> None:
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_forever()
-    except RuntimeError:
-        logger.debug("Tried starting even loop, but it is running already")
+def trigger_method(
+    sio_client: socketio.AsyncClient,
+    loop: asyncio.AbstractEventLoop,
+    access_path: str,
+    args: list[Any],
+    kwargs: dict[str, Any],
+) -> Any:
+    async def async_trigger_method() -> Any:
+        return await sio_client.call(
+            "trigger_method",
+            {
+                "access_path": access_path,
+                "args": dump(args),
+                "kwargs": dump(kwargs),
+            },
+        )
+
+    result: SerializedIconObject | None = asyncio.run_coroutine_threadsafe(
+        async_trigger_method(),
+        loop=loop,
+    ).result()
+
+    if result is not None:
+        return loads(serialized_object=result)
+
+    return None
 
 
-class Client:
+def update_value(
+    sio_client: socketio.AsyncClient,
+    loop: asyncio.AbstractEventLoop,
+    access_path: str,
+    value: Any,
+) -> Any:
+    async def set_result() -> Any:
+        return await sio_client.call(
+            "update_value",
+            {
+                "access_path": access_path,
+                "value": dump(value),
+            },
+        )
+
+    result: SerializedIconObject | None = asyncio.run_coroutine_threadsafe(
+        set_result(),
+        loop=loop,
+    ).result()
+
+    if result is not None:
+        return loads(serialized_object=result)
+
+    return None
+
+
+def get_value(
+    sio_client: socketio.AsyncClient,
+    loop: asyncio.AbstractEventLoop,
+    access_path: str,
+) -> Any:
+    async def async_get_value() -> Any:
+        return await sio_client.call(
+            "get_value",
+            {
+                "access_path": access_path,
+            },
+        )
+
+    result: SerializedIconObject = asyncio.run_coroutine_threadsafe(
+        async_get_value(),
+        loop=loop,
+    ).result()
+
+    return loads(serialized_object=result)
+
+
+class Client(pydase.Client):
     def __init__(
         self,
+        *,
         url: str,
+        block_until_connected: bool = True,
+        sio_client_kwargs: dict[str, Any] = {},
     ):
-        self._loop = asyncio.new_event_loop()
-        self._experiment_job_data: dict[int, pd.DataFrame] = {}
-        self._url = url
-        self._sio = socketio.AsyncClient()
-        self._sio.on("connect", self._handle_connect)
-        self._sio.on("disconnect", self._handle_disconnect)
-        self._sio.on("experiment_data", self._handle_experiment_data)
-        self._thread = threading.Thread(
-            target=asyncio_loop_thread, args=(self._loop,), daemon=True
+        super().__init__(
+            url=url,
+            block_until_connected=block_until_connected,
+            sio_client_kwargs=sio_client_kwargs,
         )
-        self._thread.start()
+        del self.proxy
+
+        self._experiment_job_data: dict[int, pd.DataFrame] = {}
         self.scheduler = SchedulerController(self)
-
-    def __enter__(self) -> Self:
-        self.connect()
-        return self
-
-    def __del__(self) -> None:
-        self.disconnect()
-
-    def __exit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_val: BaseException | None,
-        exc_tb: TracebackType | None,
-    ) -> None:
-        self.disconnect()
 
     async def _handle_experiment_data(self, data: ExperimentData) -> None:
         logger.debug("Updating experiment data...")
@@ -93,95 +138,42 @@ class Client:
         logger.debug(self._experiment_job_data[job_id])
 
     async def _handle_connect(self) -> None:
-        logger.debug("Connected")
+        logger.debug("Connected to '%s' ...", self._url)
 
     async def _handle_disconnect(self) -> None:
-        logger.debug("Disconnected")
+        logger.debug("Disconnected from '%s' ...", self._url)
 
-    async def _async_connect(self) -> None:
-        if not self._sio.connected:
-            await self._sio.connect(
-                self._url,
-                socketio_path="/ws/socket.io",
-                transports=["websocket"],
-                retry=False,
-            )
-
-    def connect(self) -> None:
-        connection_future = asyncio.run_coroutine_threadsafe(
-            self._async_connect(), self._loop
-        )
-        connection_future.result()
-
-    def disconnect(self) -> None:
-        connection_future = asyncio.run_coroutine_threadsafe(
-            self._async_disconnect(), self._loop
-        )
-        connection_future.result()
-
-    async def _async_disconnect(self) -> None:
-        if self._sio.connected:
-            await self._sio.disconnect()
-
-    async def _async_get_value(self, full_access_path: str) -> Any:
-        serialized_value: SerializedIconObject | None = await self._sio.call(
-            "get_value", full_access_path
-        )
-        if serialized_value is not None:
-            return loads(serialized_value)
-        return None
+    async def _setup_events(self) -> None:
+        self._sio.on("connect", self._handle_connect)
+        self._sio.on("disconnect", self._handle_disconnect)
+        self._sio.on("experiment_data", self._handle_experiment_data)
 
     def get_value(self, full_access_path: str) -> Any:
-        get_value_future = asyncio.run_coroutine_threadsafe(
-            self._async_get_value(full_access_path), self._loop
-        )
-        return get_value_future.result()
-
-    async def _async_set_value(self, full_access_path: str, new_value: Any) -> Any:
-        return await self._sio.call(
-            "update_value",
-            {
-                "access_path": full_access_path,
-                "value": dump(new_value),
-            },
+        return get_value(
+            sio_client=self._sio,
+            loop=self._loop,
+            access_path=full_access_path,
         )
 
     def set_value(self, full_access_path: str, new_value: Any) -> Any:
-        set_value_future = asyncio.run_coroutine_threadsafe(
-            self._async_set_value(full_access_path, new_value), self._loop
+        return update_value(
+            sio_client=self._sio,
+            loop=self._loop,
+            access_path=full_access_path,
+            value=new_value,
         )
-        return set_value_future.result()
-
-    async def _async_trigger_method(
-        self,
-        full_access_path: str,
-        *,
-        args: tuple[Any, ...] = (),
-        kwargs: dict[str, Any] = {},
-    ) -> Any:
-        result = await self._sio.call(
-            "trigger_method",
-            {
-                "access_path": full_access_path,
-                "args": dump(list(args)),
-                "kwargs": dump(kwargs),
-            },
-        )
-
-        if result is not None:
-            return loads(serialized_object=result)
-
-        return None
 
     def trigger_method(
         self,
         full_access_path: str,
         *,
-        args: tuple[Any, ...] = (),
+        args: list[Any] = [],
         kwargs: dict[str, Any] = {},
     ) -> Any:
-        trigger_method_future = asyncio.run_coroutine_threadsafe(
-            self._async_trigger_method(full_access_path, args=args, kwargs=kwargs),
-            self._loop,
+        return trigger_method(
+            sio_client=self._sio,
+            loop=self._loop,
+            access_path=full_access_path,
+            args=args,
+            kwargs=kwargs,
         )
-        return trigger_method_future.result()
