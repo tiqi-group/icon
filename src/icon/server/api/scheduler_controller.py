b6aa1d47769b26c2a7d47200a38350933cb100eb
Mose Müller <mosemueller@gmail.com>
2024-12-19 17:06:19 +0100

    server: updates SchedulerController to handle properly client requests

--- src/icon/server/api/scheduler_controller.py
+++ src/icon/server/api/scheduler_controller.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import pydase
 
+import icon.server.data_access.models.sqlite.scan_parameter as sqlite_scan_parameter
 from icon.server.api.models.scan_parameter import ScanParameter
 from icon.server.data_access.models.sqlite.experiment_source import ExperimentSource
 from icon.server.data_access.models.sqlite.job import Job, zurich_timezone
@@ -23,17 +24,26 @@ class SchedulerController(pydase.DataService):
         local_parameters_timestamp: datetime = datetime.now(tz=zurich_timezone),
         repetitions: int = 1,
         git_commit_hash: str | None = None,
+        auto_calibration: bool = False,
     ) -> int:
         experiment_source = ExperimentSource(experiment_id=experiment_id)
 
         experiment_source = ExperimentSourceRepository.get_or_create_experiment(
             experiment_source=experiment_source
         )
+        sqlite_scan_parameters = [
+            sqlite_scan_parameter.ScanParameter(
+                variable_id=param["parameter"],
+                scan_values=param["values"],
+                # remote_source=param.remote_source,
+            )
+            for param in scan_parameters
+        ]
         job = Job(
             experiment_source=experiment_source,
             priority=priority,
             local_parameters_timestamp=local_parameters_timestamp,
-            scan_parameters=scan_parameters,
+            scan_parameters=sqlite_scan_parameters,
             repetitions=repetitions,
             git_commit_hash=git_commit_hash,
         )
