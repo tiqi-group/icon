ae6c94e5b94d0ca198e8db432c2a356f5b15c020
Mose Müller <mosemueller@gmail.com>
2024-12-19 17:05:55 +0100

    server: replaces ScanParameter pydantic model with TypedDict

--- src/icon/server/api/models/scan_parameter.py
+++ src/icon/server/api/models/scan_parameter.py
@@ -1,7 +1,23 @@
-import pydantic
+from __future__ import annotations
+
+import sys
+from typing import Any, TypedDict
+
+from typing_extensions import NotRequired
+
+if sys.version_info < (3, 11):
+    from typing_extensions import NotRequired
+else:
+    from typing import NotRequired
 
 
-class ScanParameter(pydantic.BaseModel):
-    remote_source: str
-    variable_name: str
-    scan_values: list[float]
+class ScanParameter(TypedDict):
+    parameter: str
+    """The parameter identifier. """
+    values: dict[str, Any] | list[Any]
+    """ Either a dictionary with 'start', 'stop', and 'num_points' keys or a list of
+    explicit values. """
+    randomized: NotRequired[bool]
+    """Boolean indicating whether the values should be shuffled."""
+    reversed: NotRequired[bool]
+    """Boolean indicating whether the values should be reversed."""
