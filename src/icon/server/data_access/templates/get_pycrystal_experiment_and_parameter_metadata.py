8db9d933800a8778f7c049f9abe7f0cbde150b07
Mose Müller <mosemueller@gmail.com>
2025-02-17 07:08:53 +0100

    updates template to get pycrystal experiment and parameter metadata

--- src/icon/server/data_access/templates/get_pycrystal_experiment_and_parameter_metadata.py
+++ src/icon/server/data_access/templates/get_pycrystal_experiment_and_parameter_metadata.py
@@ -1,41 +1,17 @@
 import json
 import pkgutil
-from typing import Any, TypedDict
 
 import experiment_library.experiments
-from pycrystal.parameter_registry import ParameterMetadata
 from pycrystal.parameters import Parameter
 from pycrystal.utils.helpers import (
-    get_config_from_module_name,
-    get_experiment_instance_display_groups,
+    ExperimentDict,
+    get_experiment_metadata,
 )
-
-
-class ExperimentMetadata(TypedDict):
-    class_name: str
-    constructor_kwargs: dict[str, Any]
-    parameters: dict[str, dict[str, ParameterMetadata]]
-
-
-ExperimentDict = dict[str, dict[str, ExperimentMetadata]]
-
 
 experiments: ExperimentDict = {}
 for mod_info in pkgutil.iter_modules(experiment_library.experiments.__path__):
     experiment_module = experiment_library.experiments.__name__ + "." + mod_info.name
-    config = get_config_from_module_name(experiment_module)
-    for experiment, experiment_instance_kwargs in config["experiment_instances"]:
-        experiment_instance_name = experiment_instance_kwargs["name"]
-        experiment_identifier = (
-            f"{experiment_module}.{experiment.__name__} ({experiment_instance_name})"
-        )
-        experiments[experiment_identifier] = {
-            "class_name": experiment.__name__,
-            "constructor_kwargs": experiment_instance_kwargs,
-            "parameters": get_experiment_instance_display_groups(
-                experiment_module, experiment_instance_name
-            ),
-        }
+    experiments.update(get_experiment_metadata(experiment_module=experiment_module))
 
 parameters = {
     "all parameters": Parameter.registry.all_parameters,
