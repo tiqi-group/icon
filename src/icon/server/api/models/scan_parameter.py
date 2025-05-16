e43b0f358efcfb21aa2a7aeacd38e2541303d441
Mose Müller <mosemueller@gmail.com>
2025-05-16 14:30:16 +0200

    updates ScanParameter model

--- src/icon/server/api/models/scan_parameter.py
+++ src/icon/server/api/models/scan_parameter.py
@@ -1,23 +1,11 @@
 from __future__ import annotations
 
-import sys
 from typing import Any, TypedDict
-
-from typing_extensions import NotRequired
-
-if sys.version_info < (3, 11):
-    from typing_extensions import NotRequired
-else:
-    from typing import NotRequired
 
 
 class ScanParameter(TypedDict):
-    parameter: str
+    id: str
     """The parameter identifier. """
     values: dict[str, Any] | list[Any]
     """ Either a dictionary with 'start', 'stop', and 'num_points' keys or a list of
     explicit values. """
-    randomized: NotRequired[bool]
-    """Boolean indicating whether the values should be shuffled."""
-    reversed: NotRequired[bool]
-    """Boolean indicating whether the values should be reversed."""
