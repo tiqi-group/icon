914812719835d251c91d25df881e560480cc0216
Mose Müller <mosemueller@gmail.com>
2025-06-17 16:11:06 +0200

    removes AsyncRedisManager from sio_setup

--- src/icon/server/web_server/sio_setup.py
+++ src/icon/server/web_server/sio_setup.py
@@ -9,26 +9,16 @@ from icon.server.data_access.repositories.experiment_data_repository import (
     ExperimentData,
     ExperimentDataRepository,
 )
-from icon.server.exceptions import ValkeyUnavailableError
-from icon.server.utils.valkey import is_valkey_available, valkey_url
 
 logger = logging.getLogger(__name__)
 
 pydase_setup_sio_events = pydase.server.web_server.sio_setup.setup_sio_events
-
-if not is_valkey_available():
-    raise ValkeyUnavailableError()
-
-sio_client_manager = socketio.AsyncRedisManager(url=valkey_url())
 
 
 def setup_sio_events(
     sio: socketio.AsyncServer,
     state_manager: pydase.data_service.state_manager.StateManager,
 ) -> None:
-    assert isinstance(sio.manager, type(sio_client_manager)), (
-        "Socket.IO manager must use Redis"
-    )
     pydase_setup_sio_events(sio, state_manager)
 
     @sio.event  # type: ignore
@@ -40,4 +30,3 @@ def patch_sio_setup() -> None:
     import pydase.server.web_server.sio_setup
 
     pydase.server.web_server.sio_setup.setup_sio_events = setup_sio_events
-    pydase.server.web_server.sio_setup.sio_client_manager = sio_client_manager
