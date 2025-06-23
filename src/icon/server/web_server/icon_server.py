efe99a650851c9d509897f945d40f52634222e42
Mose Müller <mosemueller@gmail.com>
2025-06-23 14:31:01 +0200

    IconServer: removes devices_callback from post_startup hook again

--- src/icon/server/web_server/icon_server.py
+++ src/icon/server/web_server/icon_server.py
@@ -1,10 +1,8 @@
 import asyncio
 import logging
 import queue
-from typing import Any
 
 import pydase
-from pydase.utils.serialization.types import SerializedObject
 
 from icon.server.web_server.socketio_emit_queue import emit_queue
 
@@ -28,30 +26,3 @@ class IconServer(pydase.Server):
                 )
 
         asyncio.create_task(emit_worker())
-
-        # Add notification callback to observer
-        def devices_callback(
-            full_access_path: str, value: Any, cached_value_dict: SerializedObject
-        ) -> None:
-            if cached_value_dict != {} and self._loop.is_running():
-                if not full_access_path.startswith("devices._device_proxies"):
-                    return
-                logger.info(f"{full_access_path}: {value}")
-
-                async def notify() -> None:
-                    try:
-                        await sio.emit(
-                            "notify",
-                            {
-                                "data": {
-                                    "full_access_path": full_access_path,
-                                    "value": cached_value_dict,
-                                }
-                            },
-                        )
-                    except Exception as e:
-                        logger.warning("Failed to send notification: %s", e)
-
-                self._loop.create_task(notify())
-
-        self._observer.add_notification_callback(devices_callback)
