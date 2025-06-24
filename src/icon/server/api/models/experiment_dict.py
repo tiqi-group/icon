f7136b00fdb5c0b1b047e7ec4ace133f52a72e3c
Mose Müller <mosemueller@gmail.com>
2025-06-24 08:55:06 +0200

    updates ExperimentDict model and adds docstring

--- src/icon/server/api/models/experiment_dict.py
+++ src/icon/server/api/models/experiment_dict.py
@@ -11,4 +11,40 @@ class ExperimentMetadata(TypedDict):
     parameters: dict[str, dict[str, ParameterMetadata]]
 
 
-ExperimentDict = dict[str, dict[str, ExperimentMetadata]]
+ExperimentDict = dict[str, ExperimentMetadata]
+"""Dictionary mapping the unique experiment identifier to its metadata.
+
+Example:
+    ```python
+    experiment_dict: ExperimentDict = {
+        "experiment_library.experiments.my_experiment.MyExperiment (Cool Det)": {
+            "class_name": "MyExperiment",
+            "constructor_kwargs": {
+                "name": "Cool Det",
+            },
+            "parameters": {
+                "Local Parameters": {
+                    "namespace='experiment_library.experiments.my_experiment.MyExperiment.Cool Det' parameter_group='default' param_type='ParameterTypes.AMPLITUDE'": {
+                        "allowed_values": None,
+                        "default_value": 0.0,
+                        "display_name": "amplitude",
+                        "max_value": 100.0,
+                        "min_value": 0.0,
+                        "unit": "%",
+                    },
+                },
+                "ParameterGroup": {
+                    "namespace='experiment_library.globals.global_parameters' parameter_group='ParameterGroup' param_type='ParameterTypes.AMPLITUDE'": {
+                        "allowed_values": None,
+                        "default_value": 0.0,
+                        "display_name": "amplitude",
+                        "max_value": 100.0,
+                        "min_value": 0.0,
+                        "unit": "%",
+                    },
+                },
+            },
+        },
+    }
+    ```
+"""  # noqa: E501
