370bc04ff2777d02d00891e0b809c12183e2f739
Mose Müller <mosemueller@gmail.com>
2025-07-08 07:04:27 +0200

    frontend: useConfiguration hook subscribes to config changes

--- frontend/src/hooks/useConfiguration.tsx
+++ frontend/src/hooks/useConfiguration.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { runMethod } from "../socket";
+import { runMethod, socket } from "../socket";
 import { SerializedDict } from "../types/SerializedObject";
 import { deserialize } from "../utils/deserializer";
 import { Configuration } from "../types/Configuration";
@@ -8,10 +8,19 @@ export const useConfiguration = (): Configuration | null => {
   const [configuration, setConfiguration] = useState<Configuration | null>(null);
 
   useEffect(() => {
+    socket.on("config.update", (result) => {
+      console.log(result);
+      setConfiguration(result);
+    });
+
     runMethod("config.get_config", [], {}, (response) => {
       const config = deserialize(response as SerializedDict);
       setConfiguration(config);
     });
+
+    return () => {
+      socket.off("config.update");
+    };
   }, []);
 
   return configuration;
