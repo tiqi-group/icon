5b8866a53527a6103212fbd9d0a01e0fb4f53332
Mose Müller <mosemueller@gmail.com>
2025-07-18 09:29:27 +0200

    frontend: submitting a job opens a separate windows with the job viewer now

--- frontend/src/utils/submitJob.ts
+++ frontend/src/utils/submitJob.ts
@@ -1,5 +1,7 @@
 import { ScanInfoState } from "../contexts/ScanProvider";
-import { runMethod } from "../socket";
+import { authority, forwardedProto, runMethod } from "../socket";
+import { SerializedInteger } from "../types/SerializedObject";
+import { deserialize } from "./deserializer";
 
 interface ScanParameterArgument {
   id: string;
@@ -19,11 +21,25 @@ export const submitJob = (experimentId: string, scanInfoState: ScanInfoState) =>
     },
   );
 
-  runMethod("scheduler.submit_job", [], {
-    experiment_id: experimentId,
-    scan_parameters,
-    priority: scanInfoState.priority,
-    number_of_shots: scanInfoState.shots,
-    repetitions: scanInfoState.repetitions,
-  });
+  runMethod(
+    "scheduler.submit_job",
+    [],
+    {
+      experiment_id: experimentId,
+      scan_parameters,
+      priority: scanInfoState.priority,
+      number_of_shots: scanInfoState.shots,
+      repetitions: scanInfoState.repetitions,
+    },
+    (ack) => {
+      const jobId = deserialize(ack as SerializedInteger);
+      const url = `${forwardedProto}://${authority}/data/${jobId}`;
+      console.log(url);
+      window.open(
+        url,
+        "_blank",
+        "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=500,left=100,top=100",
+      );
+    },
+  );
 };
