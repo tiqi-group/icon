6a9c8a7145b4f2ff88959857f0a6d84289f2bb9e
Mose Müller <mosemueller@gmail.com>
2025-07-22 10:22:41 +0200

    SQLite: refactors sqlite db context

---
+++ src/icon/server/data_access/db_context/sqlite/__init__.py
@@ -0,0 +1,3 @@
+from icon.server.data_access.db_context.sqlite.engine import SQLITE_URL, engine
+
+__all__ = ["SQLITE_URL", "engine"]
--- src/icon/server/data_access/db_context/sqlite/engine.py
+++ src/icon/server/data_access/db_context/sqlite/engine.py
@@ -1,8 +1,6 @@
 import pydase.config
 import sqlalchemy
 
-from icon.server.data_access.models.sqlite import Base
-
 if pydase.config.OperationMode().environment == "testing":
     SQLITE_FILE = "icon_testing.db"
 elif pydase.config.OperationMode().environment == "development":
@@ -13,7 +11,3 @@ else:
 
 SQLITE_URL = f"sqlite:///{SQLITE_FILE}"
 engine = sqlalchemy.create_engine(SQLITE_URL, echo=False)
-
-
-def create_db_and_tables() -> None:
-    Base.metadata.create_all(engine)
