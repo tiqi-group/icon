ef10137fdab7e9bb5ffdf915b11d385116cc4856
Mose Müller <mosemueller@gmail.com>
2025-07-24 14:45:45 +0200

    frontend: removes DashboardLayout from job-viewer layout

--- frontend/src/layouts/job-viewer.tsx
+++ frontend/src/layouts/job-viewer.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router";
-import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { ReactRouterAppProvider } from "@toolpad/core/react-router";
 import { BRANDING } from "../App";
 
 export default function JobViewerLayout() {
   return (
     <ReactRouterAppProvider branding={BRANDING}>
-      <DashboardLayout hideNavigation>
-        <Outlet />
-      </DashboardLayout>
+      <Outlet />
     </ReactRouterAppProvider>
   );
 }
