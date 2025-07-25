64607b851cf94b60f40492a8770103719cae3bee
Mose Müller <mosemueller@gmail.com>
2025-07-25 15:23:31 +0200

    frontend: scan info state is separate for each experiment
    - moves ScanProvider from App to experiment page
    - no need to get scanInfoState in ParameterCard as param is already
    passed as a prop
    - using useScanInfoState in ScanProvider instead of a global reducer
    state
    - if useScanContext is used outside of the ScanProvider, no error will
    be thrown. Selecting parameters for a scan just doesn't work then.

--- frontend/src/hooks/useScanContext.tsx
+++ frontend/src/hooks/useScanContext.tsx
@@ -1,4 +1,18 @@
 import { useContext } from "react";
 import { ScanContext } from "../contexts/ScanContext";
+import { defaultScanInfoState } from "./useScanInfoState";
 
-export const useScanContext = () => useContext(ScanContext);
+export const useScanContext = () => {
+  const context = useContext(ScanContext);
+  if (!context) {
+    return {
+      scanInfoState: defaultScanInfoState,
+      dispatchScanInfoStateUpdate: () => {},
+      menuAnchor: { mouseX: null, mouseY: null },
+      handleRightClick: () => {},
+      handleCloseMenu: () => {},
+      scannedParamKeys: [],
+    };
+  }
+  return context;
+};
