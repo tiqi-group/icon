6e85fcee3bbfbd325ae4f5310d9fbb9a802b0711
Mose Müller <mosemueller@gmail.com>
2025-08-12 14:34:11 +0200

    sqlite: updates db context to use the config

--- src/icon/server/data_access/db_context/sqlite/engine.py
+++ src/icon/server/data_access/db_context/sqlite/engine.py
@@ -1,13 +1,22 @@
-import pydase.config
+import pathlib
+
 import sqlalchemy
 
-if pydase.config.OperationMode().environment == "testing":
-    SQLITE_FILE = "icon_testing.db"
-elif pydase.config.OperationMode().environment == "development":
-    SQLITE_FILE = "icon_dev.db"
-else:
-    SQLITE_FILE = "icon.db"
+from icon.config.config import get_config
+
+sqlite_file = get_config().databases.sqlite.file
+if sqlite_file is None:
+    raise RuntimeError(
+        "The databases.sqlite.file option in your configuration is set to None. You "
+        "have to specify a file path for the sqlite database, e.g. "
+        "/var/lib/icon/sqlite.db"
+    )
+
+sqlite_file_path = pathlib.Path(sqlite_file)
+if not sqlite_file_path.parent.exists():
+    sqlite_file_path.parent.mkdir()
 
 
-SQLITE_URL = f"sqlite:///{SQLITE_FILE}"
+SQLITE_URL = f"sqlite:///{get_config().databases.sqlite.file}"
+
 engine = sqlalchemy.create_engine(SQLITE_URL, echo=False)
