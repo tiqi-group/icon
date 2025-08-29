077858961dc274c824db6f619ceaba7bb7e7bfc0
Mose Müller <mosemueller@gmail.com>
2025-08-29 17:42:24 +0200

    ExperimentsController: only emit experiments.update when something changed

--- src/icon/server/api/experiments_controller.py
+++ src/icon/server/api/experiments_controller.py
@@ -5,6 +5,7 @@ import pydase
 from icon.server.api.models.experiment_dict import (
     ExperimentDict,
 )
+from icon.server.api.parameters_controller import get_added_removed_and_updated_keys
 from icon.server.web_server.socketio_emit_queue import emit_queue
 
 logger = logging.getLogger(__name__)
@@ -21,11 +22,15 @@ class ExperimentsController(pydase.DataService):
     def _update_experiment_metadata(self, new_experiments: ExperimentDict) -> None:
         logger.debug("Updating experiment metadata...")
 
+        added_exps, removes_exps, updated_exps = get_added_removed_and_updated_keys(
+            self._experiments, new_experiments
+        )
         self._experiments = new_experiments
 
-        emit_queue.put(
-            {
-                "event": "experiments.update",
-                "data": new_experiments,
-            }
-        )
+        if added_exps or removes_exps or updated_exps:
+            emit_queue.put(
+                {
+                    "event": "experiments.update",
+                    "data": new_experiments,
+                }
+            )
