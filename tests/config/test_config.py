923830a2d6cc1ea5e5be6d28e549b7d6d25f6480
Gerhard Bräunlich <gerhard.braeunlich@id.ethz.ch>
2025-10-08 09:52:49 +0200

    tests: Remove broken test: tests.config.test_config

--- tests/config/test_config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from icon.config.config import get_config_source
-
-
-def test_get_config_source() -> None:
-    assert get_config_source() == Path(__file__).parent.parent / "config.yaml"
