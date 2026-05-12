fc432182f8945d1bc14b5eacd0c241c591c58299
Florence Berterottiere <fberterottie@phys.ethz.ch>
2026-05-12 11:45:10 +0200

    decoding data if recieved data is in bytes

--- src/icon/server/data_access/repositories/experiment_data_repository.py
+++ src/icon/server/data_access/repositories/experiment_data_repository.py
@@ -761,6 +761,8 @@ def extract_parameter_values(
 ) -> dict[str, ParameterValue]:
     def last_value(d: h5py.Dataset) -> ParameterValue:
         ts, val = d[-1].tolist()
+        if isinstance(val, bytes):
+            val = val.decode()
         return ParameterValue(timestamp=ts.decode(), value=val)
 
     return {
