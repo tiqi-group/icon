c4ddc48b7c22dfca0cd4ba4e2cfe6146b5208e61
Anton Beitler <anton.misc@gmail.com>
2026-05-29 10:45:00 +0000

    scheduler tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,32 @@
+# ruff: noqa: PLC0415
 from __future__ import annotations
 
-import pathlib
 import logging
+import os
+import pathlib
+
 import pytest
 
 from icon.config.config_path import set_config_path
-from icon.server.api.configuration_controller import ConfigurationController
 from icon.logging import setup_logging
-
-from typing import TYPE_CHECKING, Generator
-
-if TYPE_CHECKING:
-    from sqlalchemy import Engine
+from icon.server.api.configuration_controller import ConfigurationController
 
 _testdir = pathlib.Path(__file__).parent
 _testdb = _testdir / "test_db.db"
 
-def pytest_configure(config: pytest.Config) -> None:
+def pytest_configure() -> None:
     set_config_path(_testdir / "test_config.yaml")
-    setup_logging(logging.DEBUG)
+    log_level_name = os.environ.get("TEST_LOG_LEVEL", "ERROR").upper()
+    setup_logging(getattr(logging, log_level_name, logging.DEBUG))
 
     cc = ConfigurationController()
     cc.update_config_option("databases.sqlite.file", str(_testdb))
-    # new_testdb()
 
 def new_testdb() -> None:
     _testdb.unlink(missing_ok=True)  # Remove existing test DB if it exists
     from icon.server.data_access.db_context.sqlite.engine import engine
-    assert engine.url.database is not None and str(_testdb) in engine.url.database, "SQLite engine should be configured to use the test database."
-    from icon.server.data_access.db_context.sqlite.migrations import run_migrations
+    assert engine.url.database is not None and str(_testdb) in engine.url.database, "SQLite engine should be configured to use the test database."  # noqa: PT018
+    from icon.server.data_access.db_context.sqlite.migrations import run_migrations  # noqa: I001
     run_migrations()
     engine.dispose()
 
@@ -36,13 +34,12 @@ def new_testdb() -> None:
 def new_testdb_fixture() -> None:
     new_testdb()
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(autouse=True)
 def clean_testdb() -> None:
     """Clear all tables before each test to ensure isolation."""
     from icon.server.data_access.db_context.sqlite import engine
     from icon.server.data_access.models.sqlite import Base
 
-    print("Cleaning test database...")
     with engine.begin() as conn:
         for table in reversed(Base.metadata.sorted_tables):
             conn.execute(table.delete())
--- tests/server/scheduler/test_scheduler.py
+++ tests/server/scheduler/test_scheduler.py
@@ -1,35 +1,43 @@
 from __future__ import annotations
-import queue
-import threading
-import pytest
+
 import asyncio
 import multiprocessing
+import queue
+import threading
+from datetime import UTC, datetime
+from typing import TYPE_CHECKING, Any
 
-from datetime import datetime, UTC
+import pytest
 
-from icon.server.data_access.repositories.experiment_source_repository import ExperimentSourceRepository
+from icon.server.data_access.models.enums import JobRunStatus, JobStatus
 from icon.server.data_access.models.sqlite import ExperimentSource
-
-from icon.server.scheduler.scheduler import Scheduler, initialise_job_tables
-
+from icon.server.data_access.models.sqlite.job import Job
+from icon.server.data_access.repositories.experiment_source_repository import (
+    ExperimentSourceRepository,
+)
 from icon.server.data_access.repositories.job_repository import JobRepository
 from icon.server.data_access.repositories.job_run_repository import JobRunRepository
 from icon.server.data_access.repositories.job_transaction import JobTransaction
-from icon.server.data_access.models.enums import JobStatus, JobRunStatus
-from icon.server.data_access.models.sqlite.job import Job
+from icon.server.scheduler.scheduler import Scheduler, initialise_job_tables
 
-from typing import TYPE_CHECKING, Any, Generator, AsyncGenerator, List, Callable, Sequence
 if TYPE_CHECKING:
-    from icon.server.pre_processing.task import PreProcessingTask
-    from icon.server.data_access.models.sqlite.job_run import JobRun
+    from collections.abc import AsyncGenerator, Generator
+
     from pytest_mock import MockerFixture
 
+    from icon.server.pre_processing.task import PreProcessingTask
+
 DEFAULT_WAIT_FOR_JOB_HANDLED_TIMEOUT = 2  # seconds
+TEST_EXPERIMENT_ID = "test_experiment_id"
+
+class IntentionalError(Exception):
+    """Custom exception class for intentional errors for testing purposes."""
 
 class SchedulerTestFixture:
+
     def __init__(self) -> None:
         self.experiment_source = ExperimentSourceRepository.get_or_create_experiment(
-            experiment_source=ExperimentSource(experiment_id="test_experiment_id")
+            experiment_source=ExperimentSource(experiment_id=TEST_EXPERIMENT_ID)
         )
         self.ppq : queue.PriorityQueue[PreProcessingTask] = queue.PriorityQueue()
         self.scheduler = Scheduler(pre_processing_queue=self.ppq)
@@ -41,16 +49,14 @@ class SchedulerTestFixture:
         self.thread_running = True
 
     def stop(self) -> None:
-        if self.thread_running == False:
+        if not self.thread_running:
             return
         self.scheduler.stop()
         self.thread.join()
         self.thread_running = False
 
-    def insert_jobs(self, job_cnt:int=10, **kwargs:Any) -> List[Job]:
-        jobs = []
-        for _ in range(job_cnt):
-            jobs.append(JobRepository.submit_job(job=Job(**dict(
+    def insert_jobs(self, job_cnt:int=10, **kwargs:Any) -> list[Job]:
+        return [ JobRepository.submit_job(job=Job(**dict(  # noqa: C408
                     experiment_source=self.experiment_source,
                     priority=20,
                     local_parameters_timestamp=datetime.now(tz=UTC),
@@ -60,19 +66,19 @@ class SchedulerTestFixture:
                     number_of_shots=50,
                     auto_calibration=False,
                     debug_mode=True
-                ) | kwargs)))
-        return jobs
+                ) | kwargs))
+            for _ in range(job_cnt) ]
 
     async def wait_for_jobs_handled(self, timeout:int = DEFAULT_WAIT_FOR_JOB_HANDLED_TIMEOUT) -> None:
         async def wait_task() -> None:
-            while len(JobRepository.get_jobs_by_status_and_timeframe(
+            while len(JobRepository.get_jobs_by_status_and_timeframe(  # noqa: ASYNC110
                     status=JobStatus.SUBMITTED
                 )) > 0:
                 await asyncio.sleep(0.1)
         try:
             await asyncio.wait_for(wait_task(), timeout=timeout)  # Wait until all jobs are handled
         except asyncio.TimeoutError:
-            assert False, "Timeout reached while waiting for all submitted jobs to be handled by the scheduler."
+            pytest.fail("Timeout reached while waiting for all submitted jobs to be handled by the scheduler.")
 
     def assert_job_status(self, job_id:int, expected_job_status: JobStatus, expected_job_run_status: JobRunStatus|None = None) -> None:
         job = JobRepository.get_job_by_id(job_id=job_id)
@@ -83,27 +89,23 @@ class SchedulerTestFixture:
 
     async def _consume_ppq_check_priority(self, num_items_expected: int, timeout:int = DEFAULT_WAIT_FOR_JOB_HANDLED_TIMEOUT) -> AsyncGenerator[PreProcessingTask, None]:
         async def wait_task() -> None:
-            while self.ppq.qsize() < num_items_expected:
+            while self.ppq.qsize() < num_items_expected:  # noqa: ASYNC110
                 await asyncio.sleep(0.1)
 
         try:
             await asyncio.wait_for(wait_task(), timeout=timeout)  # Wait until all tasks are added to the pre-processing queue
         except asyncio.TimeoutError:
-            assert False, "Scheduler did not add all tasks to the pre-processing queue within the expected time."
+            pytest.fail("Timeout reached while waiting for all tasks to be added to the pre-processing queue.")
 
-        try:
-            last_priority = float('-inf')
-            while not self.ppq.empty():
-                pptask = self.ppq.get()
-                assert pptask.priority >= last_priority, "Tasks retrieved from pre-processing queue are not in correct priority order."
-                last_priority = pptask.priority
-                yield pptask
-            assert self.ppq.empty(), "Pre-processing queue should be empty after retrieving tasks."
-        except queue.Empty:
-            assert False, "Expecting more tasks in the pre-processing queue, but it was empty."
+        last_priority = float("-inf")
+        while not self.ppq.empty():
+            pptask = self.ppq.get()
+            assert pptask.priority >= last_priority, "Tasks retrieved from pre-processing queue are not in correct priority order."
+            last_priority = pptask.priority
+            yield pptask
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture # default scope: function
 def scheduler_fixture(request: pytest.FixtureRequest) -> Generator[SchedulerTestFixture, None, None]:
     start_thread = getattr(request, "param", True)
     fixture = SchedulerTestFixture()
@@ -114,25 +116,24 @@ def scheduler_fixture(request: pytest.FixtureRequest) -> Generator[SchedulerTest
 
 @pytest.mark.asyncio
 async def test_scheduler_process_jobs(scheduler_fixture: SchedulerTestFixture) -> None:
-    """
-        Create Jobs with varying priorities and let them be processed by the scheduler.
-        Check database state and pre-processing queue order to ensure correct processing and priority handling.'''
-    """
+    """Create Jobs with varying priorities and let them be processed by the scheduler and check database state and pre-processing queue order to ensure consistency."""
+    _num_jobs = 10
+    for k in range(_num_jobs):
+        scheduler_fixture.insert_jobs(job_cnt=1, priority=10+k*(2*(k%2)-1)) # Insert jobs with alternating higher and lower priorities
 
-    NUM_JOBS = 10
-    for k in range(NUM_JOBS):
-        scheduler_fixture.insert_jobs(job_cnt=1, priority=10 + k*(2*(k%2)-1))
-
-    async for pptask in scheduler_fixture._consume_ppq_check_priority(num_items_expected=NUM_JOBS):
+    async for pptask in scheduler_fixture._consume_ppq_check_priority(num_items_expected=_num_jobs):
         scheduler_fixture.assert_job_status(pptask.job.id, JobStatus.PROCESSING, JobRunStatus.PENDING)
+        assert pptask.job.experiment_source.experiment_id == TEST_EXPERIMENT_ID, "Experiment source ID should match"
+        assert len(pptask.job.scan_parameters) == 0, "Scan parameters list expted to be empty"
 
 @pytest.mark.asyncio
 async def test_scheduler_queue_exception(mocker: MockerFixture, scheduler_fixture: SchedulerTestFixture) -> None:
+    """Test the exception handling when inserting into the pre-processing queue. Jobs are expected to be marked as PROCESSED and their corresponding JobRuns as FAILED."""
     real_put = scheduler_fixture.ppq.put
 
     # Inject an Exception when scheduler tries to put to the pre-processing queue
     def failing_put() -> None:
-        raise Exception("Intentional exception when adding task to pre-processing queue")
+        raise IntentionalError("Intentional exception when adding task to pre-processing queue")
     mocker.patch.object(scheduler_fixture.ppq, "put", side_effect=failing_put)
 
     failed_jobs = scheduler_fixture.insert_jobs(job_cnt=10)
@@ -145,78 +146,66 @@ async def test_scheduler_queue_exception(mocker: MockerFixture, scheduler_fixtur
     # Restore pre-processing queue
     mocker.patch.object(scheduler_fixture.ppq, "put", real_put)
 
-    NUM_JOBS = 1
-    scheduler_fixture.insert_jobs(job_cnt=NUM_JOBS)
+    _num_jobs = 1
+    scheduler_fixture.insert_jobs(job_cnt=_num_jobs)
 
     # The pre processing queue should contain the single new task
-    async for pptask in scheduler_fixture._consume_ppq_check_priority(num_items_expected=NUM_JOBS):
+    async for pptask in scheduler_fixture._consume_ppq_check_priority(num_items_expected=_num_jobs):
         scheduler_fixture.assert_job_status(pptask.job.id, JobStatus.PROCESSING, JobRunStatus.PENDING)
 
     # No further jobs in SUBMITTED status expected
     assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.SUBMITTED)) == 0, "Expecting no jobs left in SUBMITTED status after processing."
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize("scheduler_fixture", [False], indirect=True) # Don't start the scheduler thread
 async def test_scheduler_db_exceptions(mocker: MockerFixture, scheduler_fixture: SchedulerTestFixture) -> None:
-    real_get_jobs = JobRepository.get_jobs_by_status_and_timeframe
-    real_insert_run = JobTransaction.insert_run_from_jobid
+    assert not scheduler_fixture.thread_running, "Scheduler thread should not be running for this test"
 
-    # Inject an Exception on first 5 occurrences when scheduler tries to retrieve submitted jobs
-    fail_get_jobs_cnt = multiprocessing.Value('i', 0)
-    def failing_get_jobs(*args:Any, **kwargs:Any) -> Sequence[Job]:
-        nonlocal fail_get_jobs_cnt
-        fail_get_jobs_cnt.value += 1
-        if fail_get_jobs_cnt.value <= 2:
-            raise Exception("Intentional exception when fetching submitted jobs")
-        else:
-            return real_get_jobs(*args, **kwargs)
+    _num_jobs = 10
+    scheduler_fixture.insert_jobs(job_cnt=_num_jobs)
+    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.SUBMITTED)) == _num_jobs, "Expecting all inserted jobs to be in SUBMITTED status before processing"
 
-    mocker.patch.object(JobRepository, "get_jobs_by_status_and_timeframe", side_effect=failing_get_jobs)
+    for patch_class, patch_method in [
+        (JobRepository, "get_jobs_by_status_and_timeframe"),
+        (JobTransaction, "insert_run_from_jobid")
+    ]:
+        # Inject an Exception when scheduler tries to fetch submitted jobs
+        m = mocker.patch.object(patch_class, patch_method, side_effect=IntentionalError(f"Intentional exception on {patch_method}.{patch_method}"))
+        scheduler_fixture.scheduler._handle_submitted_jobs()
+        m.assert_called()
+        mocker.stopall()
+        assert scheduler_fixture.ppq.empty(), "Pre-processing queue should be empty when database fetch fails"
+        assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.SUBMITTED)) == _num_jobs, "Expecting all jobs to remain in SUBMITTED status when database fetch fails"
 
-    fail_insert_cnt = multiprocessing.Value('i', 0)
-    def failing_insert_run(*args:Any, **kwargs:Any) -> tuple[Job, JobRun]:
-        nonlocal fail_insert_cnt
-        fail_insert_cnt.value += 1
-        if fail_insert_cnt.value <= 5:
-            raise Exception("Intentional exception when creating JobRun")
-        else:
-            return real_insert_run(*args, **kwargs)
+    # With exceptions resolved, scheduler should be able to process jobs and add tasks to the pre-processing queue
+    scheduler_fixture.scheduler._handle_submitted_jobs()
 
-    mocker.patch.object(JobTransaction, "insert_run_from_jobid", side_effect=failing_insert_run)
-
-
-    NUM_JOBS = 10
-    scheduler_fixture.insert_jobs(job_cnt=NUM_JOBS)
-
-    async for pptask in scheduler_fixture._consume_ppq_check_priority(num_items_expected=NUM_JOBS, timeout=5):
+    async for pptask in scheduler_fixture._consume_ppq_check_priority(num_items_expected=_num_jobs, timeout=2):
         scheduler_fixture.assert_job_status(pptask.job.id, JobStatus.PROCESSING, JobRunStatus.PENDING)
 
 
-
 @pytest.mark.asyncio
-@pytest.mark.parametrize("scheduler_fixture", [False], indirect=True)
-async def test_job_table_init(mocker: MockerFixture, scheduler_fixture: SchedulerTestFixture) -> None:
-    assert scheduler_fixture.thread_running == False, "Scheduler thread should not be running for this test"
-    ALL_JOBRUN_STATUSES = [v for v in JobRunStatus]
+@pytest.mark.parametrize("scheduler_fixture", [False], indirect=True)  # Don't start the scheduler thread
+async def test_job_table_init(scheduler_fixture: SchedulerTestFixture) -> None:
+    assert not scheduler_fixture.thread_running, "Scheduler thread should not be running for this test"
 
     initialise_job_tables()
     assert len(JobRepository.get_jobs_by_status_and_timeframe()) == 0, "Expecting no jobs to be present at startup"
-    assert len(JobRunRepository.get_runs_by_status(status=ALL_JOBRUN_STATUSES)) == 0, "Expecting no job runs to be present at startup"
+    assert len(JobRunRepository.get_runs_by_status(status=list(JobRunStatus))) == 0, "Expecting no job runs to be present at startup"
 
-    NUM_JOBS = 10
+    _num_jobs = 10
     # Inserting jobs puts them in SUBMITTED status
-    scheduler_fixture.insert_jobs(job_cnt=NUM_JOBS)
+    scheduler_fixture.insert_jobs(job_cnt=_num_jobs)
     initialise_job_tables()
-    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.SUBMITTED)) == NUM_JOBS, "Expecting all submitted jobs to remain in SUBMITTED status after initialization"
+    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.SUBMITTED)) == _num_jobs, "Expecting all submitted jobs to remain in SUBMITTED status after initialization"
 
     # Process submitted jobs. Jobs should switch to PROCESSING.
     scheduler_fixture.scheduler._handle_submitted_jobs()
-    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.PROCESSING)) == NUM_JOBS, "Expecting all submitted jobs switch to PROCESSING status"
-    assert len(JobRunRepository.get_runs_by_status(status=JobRunStatus.PENDING)) == NUM_JOBS, "JobRun with PENDING status should be created for each job"
+    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.PROCESSING)) == _num_jobs, "Expecting all submitted jobs switch to PROCESSING status"
+    assert len(JobRunRepository.get_runs_by_status(status=JobRunStatus.PENDING)) == _num_jobs, "JobRun with PENDING status should be created for each job"
     # All Proccessed Jobs should be switched to PROCESSED by the initialization
     initialise_job_tables()
-    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.PROCESSED)) == NUM_JOBS, "Expecting all submitted jobs switch to PROCESSING status"
-    assert len(JobRepository.get_jobs_by_status_and_timeframe()) == NUM_JOBS, "Total job count should remain constant"
-    assert len(JobRunRepository.get_runs_by_status(status=JobRunStatus.CANCELLED)) == NUM_JOBS, "Expecting all job runs to be marked as CANCELLED by initialization"
-    assert len(JobRunRepository.get_runs_by_status(status=ALL_JOBRUN_STATUSES)) == NUM_JOBS, "Total job run count should remain constant"
-
-
+    assert len(JobRepository.get_jobs_by_status_and_timeframe(status=JobStatus.PROCESSED)) == _num_jobs, "Expecting all submitted jobs switch to PROCESSING status"
+    assert len(JobRepository.get_jobs_by_status_and_timeframe()) == _num_jobs, "Total job count should remain constant"
+    assert len(JobRunRepository.get_runs_by_status(status=JobRunStatus.CANCELLED)) == _num_jobs, "Expecting all job runs to be marked as CANCELLED by initialization"
+    assert len(JobRunRepository.get_runs_by_status(status=list(JobRunStatus))) == _num_jobs, "Total job run count should remain constant"
