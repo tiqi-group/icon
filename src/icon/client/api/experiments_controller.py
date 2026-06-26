47fb344297c132126d475f6744683e2ad8ade407
Gerhard Bräunlich <gerhard.braeunlich@id.ethz.ch>
2026-06-26 16:52:30 +0200

    refactor(sqlite): confine side effect of get_config to read timezone into own method

--- src/icon/client/api/experiments_controller.py
+++ src/icon/client/api/experiments_controller.py
@@ -5,13 +5,14 @@ import logging
 import time
 from collections import Counter
 from dataclasses import dataclass
-from datetime import datetime
 from typing import TYPE_CHECKING, Any, TypedDict
 
 from icon.server.api.models.experiment_dict import ExperimentMetadata
-from icon.server.data_access.models.sqlite.job import timezone
+from icon.server.data_access.models.sqlite.now import now
 
 if TYPE_CHECKING:
+    from datetime import datetime
+
     from icon.client.client import Client
     from icon.server.api.models.experiment_dict import (
         ExperimentDict,
@@ -399,7 +400,7 @@ class ExperimentProxy:
         if scan_parameters is None:
             scan_parameters = []
         if local_parameters_timestamp is None:
-            local_parameters_timestamp = datetime.now(tz=timezone)
+            local_parameters_timestamp = now()
         job_id: int = self._client.trigger_method(
             "scheduler.submit_job",
             kwargs={
