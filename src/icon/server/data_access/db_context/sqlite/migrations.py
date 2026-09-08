2d58699a6a6ae4b686c74bb6630fdd99eaea57dd
Anton Beitler <anton.beitler@qendra.tech>
2026-09-08 15:06:59 +0000

    type: ignore missing custom flag

--- src/icon/server/data_access/db_context/sqlite/migrations.py
+++ src/icon/server/data_access/db_context/sqlite/migrations.py
@@ -16,6 +16,6 @@ def run_migrations() -> None:
     alembic_cfg.set_main_option("script_location", str(base_path / "alembic"))
 
     # Custom flag to prevent env.py from overriding logging
-    alembic_context._from_application = True
+    alembic_context._from_application = True  # type: ignore[attr-defined]
 
     command.upgrade(alembic_cfg, "head")
